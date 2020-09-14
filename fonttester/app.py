0c1d7fffd70b170202b01c7afce3b4261d59e1d0
tobiasjcat <paul@croft.club>
2020-09-14 14:05:10 -0700

    Added caesar fonts

--- fonttester/app.py
+++ fonttester/app.py
@@ -3,7 +3,7 @@
 #Paul Croft
 #September 10, 2020
 
-from bottle import get, run, static_file, template
+from bottle import get, response, run, static_file, template
 
 import string
 
@@ -11,6 +11,16 @@ import string
 @get("/static/<sfile>")
 def staticassets(sfile):
     return static_file(sfile, root="static")
+
+@get("/ttfs/<ttfsfile>")
+def ttfassets(ttfsfile):
+    return static_file(ttfsfile, root="ttfs")
+
+
+@get("/rotfonts.css")
+def rotfonts_file():
+    response.content_type = 'text/css'
+    return template("templates/rotfonts.css")
 
 @get("/")
 @get("/index")
